3d5523efdb51f7c4d26b023cdfb4495de2614a3a
Nikhil <nikhil_n@live.com>
2019-10-07 08:38:54 +0530

    Adding the questionary page.

--- lib/disclaimer.dart
+++ lib/disclaimer.dart
@@ -13,10 +13,27 @@ class DisclaimerScreen extends StatefulWidget {
 }
 
 String disclaimer =
-    "Disclaimer: I commit that i will give the details \n best to my knowledge";
+    //"Disclaimer: I commit that i will give the details \n best to my knowledge";
+
+    "I accept that our company will handle personal data\n"
+    "given by me and may check my medical history. Our\n"
+    "company handles and may disclose data in accordance\n"
+    "with legislation governing the handling of personal\n"
+    "data and makes sure all personal data is handled \n"
+    "bank secrecy. Personal data is handled in order to\n"
+    "provide services and for the purposes of the bank\n"
+    "operations and risk management.\n"
+    "Data is collected from the customer, from registers \n"
+    "maintained by the authorities, from credit information \n"
+    "and payment default registers and from other reliable\n"
+    "sources. Company also uses its customer register for \n"
+    "direct marketing to its customers.Customers are entit-\n"
+    "led to forbid direct marketing.\n"
+    "Read more about our company privacy policy.";
+bool isChecked =false;
 
 class DisclaimerScreenWidged extends State<DisclaimerScreen> {
-  get selected => false;
+  get selecisCheckedted => false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,12 +44,12 @@ class DisclaimerScreenWidged extends State<DisclaimerScreen> {
           elevation: 0.4,
         ),
         body: Stack(fit: StackFit.expand, children: <Widget>[
-          Container(decoration: BoxDecoration(color: Colors.blueAccent)),
+          Container(decoration: BoxDecoration(color: Colors.white10)),
           Column(
             children: <Widget>[
-              Expanded(flex: 4, child: Column()),
+              Expanded(flex: 3, child: Column()),
               Expanded(
-                  flex: 2,
+                  flex: 4,
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
@@ -41,23 +58,31 @@ class DisclaimerScreenWidged extends State<DisclaimerScreen> {
                             children: <Widget>[
                               Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[Checkbox(value: false, onChanged: (state){ onChangeCheckBox(state);})]),
+                                  children: <Widget>[
+                                    Checkbox(
+                                        value: isChecked,
+                                        activeColor: Colors.blue,
+                                        onChanged: (state) {
+                                          setState(() {
+                                            isChecked = state;
+                                          });
+                                          onChangeCheckBox(state);
+                                        })
+                                  ]),
                               Column(
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: <Widget>[Text(disclaimer)])
                             ]),
                         Container(
                           margin: const EdgeInsets.only(top: 10.0),
-                          padding: EdgeInsets.only(top: 4.0, bottom: 4.0, right: 10.0, left: 10.0),
+                          padding: EdgeInsets.only(
+                              top: 4.0, bottom: 4.0, right: 10.0, left: 10.0),
                           child: FloatingActionButton.extended(
-                            onPressed: () {
-                              buttonPressed();
-                              },
+                              onPressed: !isChecked ? null : () { buttonPressed();},
                               key: Key("buttonKey"),
-                            label: Text('Approve'),
-                            icon: Icon(Icons.thumb_up),
-                            backgroundColor: Colors.pink
-                          ),
+                              label: Text('Approve'),
+                              icon: Icon(Icons.thumb_up),
+                              backgroundColor: Colors.pink),
                         ),
                       ]))
             ],
@@ -66,17 +91,14 @@ class DisclaimerScreenWidged extends State<DisclaimerScreen> {
   }
 
   buttonPressed() {
-    print("button clicked");
-
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => HomeScreen()));
   }
 
-  buttonActive() {
-
-  }
+  buttonActive() {}
 
   onChangeCheckBox(bool state) {
+
     buttonActive();
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,15 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:insurance_underwriting/myAppBar.dart';
+import 'package:flutter_dialogflow/flutter_dialogflow.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return new HomeScreenWidget();
+    return new _MyHomePageState();;
   }
 }
+/*
 
 class HomeScreenWidget extends State<HomeScreen>{
   @override
@@ -22,21 +21,180 @@ class HomeScreenWidget extends State<HomeScreen>{
         elevation: 0.4,
       ),
       body: Stack( fit:StackFit.expand, children: <Widget>[
-        Container(
-
-
-        ),
-
+        Container(decoration: BoxDecoration(color: Colors.blueAccent)),
+        Column( mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
 
 
 
+          ],
+
+
+      )
 
       ],
-
-
-
       ) ,
 
+    );
+  }
+}*/
+
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+/* https://github.com/VictorRancesCode/flutter_dialogflow/blob/master/example/lib/main.dart */
+
+class _MyHomePageState extends State<HomeScreen> {
+
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void Response(query) async {
+    _textController.clear();
+    Dialogflow dialogflow =Dialogflow(token: "3a33a3a4f3b447ca8dcdd6793fab29c8");
+    AIResponse response = await dialogflow.sendQuery(query);
+    ChatMessage message = new ChatMessage(
+      text: response.getMessageResponse(),
+      name: "Alf the Bot",
+      type: false,
+    );
+
+    FlutterTts flutterTts = new FlutterTts();
+
+    flutterTts.speak(response.getMessageResponse());
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+      name: "Me",
+      type: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    Response(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Underwriting"),
+      ),
+      body: new Column(children: <Widget>[
+        new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )),
+        new Divider(height: 1.0),
+        new Container(
+          decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+          child: _buildTextComposer(),
+        ),
+      ]),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      new Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: new CircleAvatar(child: new Image.asset("img/placeholder.png")),
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(this.name, style:new TextStyle(fontWeight:FontWeight.bold )),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text(this.name, style: Theme.of(context).textTheme.subhead),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+      new Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: new CircleAvatar(child: new Text(this.name[0])),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
     );
   }
 }
